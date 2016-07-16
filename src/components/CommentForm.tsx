2e278c5655b1436ab1a3bb45d73651f1d80482bc
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-16 12:25:34 +0800

    綁定表單的事件
    我們用了 state 來綁定表單上的輸入元件 (可以想想為什麼不用 props)。值得注
    意的是綁定事件的時候，要用 closure 或是 .bind(this)。這是因為
    React.createComponent() 會自動幫你做這件事，但我們用的不是它QQ
    
    另外，事件綁定的標記 onXxx 要注意大小寫，細節可以去看 react 的文件。

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,11 +1,52 @@
 import * as React from "react";
 
-export default class CommentForm extends React.Component<{}, {}> {
+export interface CommentFormState {
+    author?: string;
+    text?: string;
+}
+
+export default class CommentForm extends React.Component<{}, CommentFormState> {
+    constructor(props?: {}, context?: any) {
+	super(props, context);
+
+	this.state = {
+	    author: "",
+	    text: ""
+	};
+    }
+
+    handleAuthorChange(e:Event) {
+	this.setState({author: (e.target as HTMLInputElement).value});
+    }
+
+    handleTextChange(e:Event) {
+	this.setState({text: (e.target as HTMLInputElement).value});
+    }
+
+    handleSubmit(e:Event) {
+	e.preventDefault();
+	var author = this.state.author.trim();
+	var text = this.state.text.trim();
+	if (!text || !author) {
+	    return;
+	}
+	// TODO: send request to the server
+	this.setState({author: '', text: ''});
+    }
+
     render() {
 	return (
-	    <form className="commentForm">
-		<input type="text" placeholder="Your name" />
-		<input type="text" placeholder="Say something..." />
+	    <form className="commentForm" onSubmit={this.handleSubmit.bind(this)}>
+		<input
+		    type="text"
+		    placeholder="Your name"
+		    value={this.state.author}
+		    onChange={this.handleAuthorChange.bind(this)} />
+		<input
+		    type="text"
+		    placeholder="Say somethind..."
+		    value={this.state.text}
+		    onChange={this.handleTextChange.bind(this)} />
 		<input type="submit" value="Post" />
 	    </form>
 	);
