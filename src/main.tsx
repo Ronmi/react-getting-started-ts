2cb3646e3a01aafae56c9013063656051ecd4b79
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-16 11:42:46 +0800

    修正一個遺漏的,

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import CommentBox from "./components/CommentBox.tsx";
 import {data} from "./comment";
 
 ReactDOM.render(
-    <CommentBox url="http://127.0.0.1:4000/api/comments" />
+    <CommentBox url="http://127.0.0.1:4000/api/comments" />,
     document.getElementById('content')
 );
