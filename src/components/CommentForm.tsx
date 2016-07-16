3a2672448d654f158f0a36e7d5df5c047efaed20
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-16 12:05:00 +0800

    產生表單
    不多說，先弄個簡單的表單，看得到東西才好寫 code

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -3,9 +3,11 @@ import * as React from "react";
 export default class CommentForm extends React.Component<{}, {}> {
     render() {
 	return (
-	    <div className="commentForm">
-		Hello, world! I am a CommentForm.
-	    </div>
+	    <form className="commentForm">
+		<input type="text" placeholder="Your name" />
+		<input type="text" placeholder="Say something..." />
+		<input type="submit" value="Post" />
+	    </form>
 	);
     }
 };
