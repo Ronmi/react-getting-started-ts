55d0dfbf8c284536772ba7a239f54a8002a6945b
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-18 17:24:00 +0800

    試玩 enzyme 的 render
    初始設定有點麻煩，還好有 boilerplate 可以用，詳見
    https://github.com/lelandrichardson/enzyme-example-mocha

--- test.bootstrap.js
+++ test.bootstrap.js
@@ -1,1 +1,22 @@
 global.Remarkable = require("remarkable");
+
+// These lines initializes a vietral environment for react to use.
+// Copied from https://github.com/lelandrichardson/enzyme-example-mocha/blob/master/test/.setup.js
+var jsdom = require("jsdom").jsdom;
+var exposedProperties = ["window", "navigator", "document"];
+
+global.document = jsdom("");
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;
+// End of environment initializing.
--- tests/components/CommentTest.tsx
+++ tests/components/CommentTest.tsx
@@ -1,15 +1,21 @@
 /// <reference path="../../typings/globals/mocha/index.d.ts" />
 
 import * as React from "react";
-import { shallow } from "enzyme";
+import { shallow, render } from "enzyme";
 import Comment from "../../src/components/Comment";
 import * as model from "../../src/comment";
 import { expect } from "chai";
 
 describe("<Comment />", () => {
-    const wrapper = shallow(<Comment author="test">*test* html</Comment>);
+    const comment = <Comment author="test">*test* html</Comment>;
     it("has exactly one h2 and one span.", () => {
+	const wrapper = shallow(comment);
         expect(wrapper.find("h2")).to.have.length(1);
         expect(wrapper.find("span")).to.have.length(1);
+    });
+
+    it("has exactly one italic word", () => {
+	const wrapper = render(comment);
+	expect(wrapper.find("em")).to.have.length(1);
     });
 });
