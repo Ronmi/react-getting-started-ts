081ec3f91a69669b8a25d3f1755147ebca5c38c7
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-18 22:20:37 +0800

    完成 Comment component 的測試，同時移除多餘的引入

--- tests/components/CommentTest.tsx
+++ tests/components/CommentTest.tsx
@@ -3,10 +3,9 @@
 import * as React from "react";
 import { shallow, render } from "enzyme";
 import Comment from "../../src/components/Comment";
-import * as model from "../../src/comment";
 import { expect } from "chai";
 
-describe("<Comment />", () => {
+describe("<Comment author=\"test\">*test* html</Comment>", () => {
     const comment = <Comment author="test">*test* html</Comment>;
     it("has exactly one h2 and one span.", () => {
 	const wrapper = shallow(comment);
@@ -14,8 +13,18 @@ describe("<Comment />", () => {
         expect(wrapper.find("span")).to.have.length(1);
     });
 
-    it("has exactly one italic word", () => {
+    it("has author name in the h2.", () => {
+	const wrapper = shallow(comment);
+        expect(wrapper.find("h2").text()).to.equal("test");
+    });
+
+    it("executes remarkable on comment and puts result in the span.", () => {
 	const wrapper = render(comment);
-	expect(wrapper.find("em")).to.have.length(1);
+	expect(wrapper.find("span em")).to.have.length(1);
+    });
+
+    it("has comment in the span (ignore heading and trailing space characters due to HTML spec).", () => {
+	const wrapper = render(comment);
+	expect(wrapper.find("span").text().trim()).to.equal("test html");
     });
 });
